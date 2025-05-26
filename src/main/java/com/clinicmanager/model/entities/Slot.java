86513b3ee89f690d21c38d55b3f36bbbe809889f
mykhailo tsonyev <th@mykhailos-MacBook-Air.local>
2025-05-26 20:39:43 +0200

    Medical card Appointmemt field added WIP

--- src/main/java/com/clinicmanager/model/entities/Slot.java
+++ src/main/java/com/clinicmanager/model/entities/Slot.java
@@ -1,7 +1,9 @@
 package com.clinicmanager.model.entities;
 
 import com.clinicmanager.gui.AppContext;
+import com.clinicmanager.repository.AppointmentRepository;
 import com.clinicmanager.repository.SlotRepository;
+import com.clinicmanager.repository.AppointmentRepository;
 
 import java.time.LocalDate;
 
@@ -11,6 +13,9 @@ public class Slot {
     private final LocalDate date;
     private final TimeRange timeRange;
     private final SlotRepository slotRepository;
+    private final AppointmentRepository appointmentRepository;
+    private Appointment appointment;
+
 
     public Slot(int id, int scheduleId, LocalDate date, TimeRange timeRange) {
         this.id = id;
@@ -18,6 +23,7 @@ public class Slot {
         this.date = date;
         this.timeRange = timeRange;
         this.slotRepository = AppContext.getInstance().getRepositories().slots;
+        this.appointmentRepository = AppContext.getInstance().getRepositories().appointments;
     }
 
     public Slot(int scheduleId, LocalDate date, TimeRange timeRange) {
